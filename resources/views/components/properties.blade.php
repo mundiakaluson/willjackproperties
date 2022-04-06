534ca6a41ccb7f1ad9c328b299bc2be6cf308add
Jackson Mundia <mundiakaluson@gmail.com>
2022-04-06 13:29:26 +0300

    some stuff

--- resources/views/components/properties.blade.php
+++ resources/views/components/properties.blade.php
@@ -21,7 +21,7 @@
         <div class="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
           <img src="{{ url('storage/' . $str_clean[0]) }}"
             alt="Tall slender porcelain bottle with natural clay textured body and cork stopper."
-            class="w-full h-full object-center object-cover group-hover:opacity-75">
+            class="w-full h-1/2 object-center object-cover group-hover:opacity-75">
         </div>
         <h3 class="mt-4 text-sm text-gray-700">
           {{ $property->name }}
